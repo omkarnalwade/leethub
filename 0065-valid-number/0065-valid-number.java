07b20007530ea6e56e56852125f842e3c32351bb
Omkar Nalwade <omkarnalwade5@gmail.com>
2026-08-25 03:13:34 +0530

    Time: 11 ms (5.97%), Space: 47.4 MB (5.55%) - LeetHub

--- 0065-valid-number/0065-valid-number.java
+++ 0065-valid-number/0065-valid-number.java
@@ -1,29 +1,7 @@
+import java.util.regex.*;
 class Solution {
     public boolean isNumber(String s) {
-        boolean digitseen=false,eseen=false,dotseen=false;
-        int countPlusMinus=0;
-
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(Character.isDigit(ch)) digitseen=true;
-            else if(ch=='+' || ch=='-'){
-                if(countPlusMinus==2) return false;
-                if(i>0 && s.charAt(i-1) != 'e' && s.charAt(i-1) != 'E') return false;
-                if(i==s.length()-1) return false;
-                
-                countPlusMinus++;
-            }
-            else if(ch=='.'){
-                if(eseen || dotseen ) return false;
-                if(i==s.length()-1 && !digitseen) return false;
-                dotseen=true;
-            }
-            else if(ch=='E' || ch=='e'){
-                if(eseen || i==s.length()-1 || !digitseen) return false;
-                eseen=true;
-            }
-            else return false;
-        }
-        return true;
+        String regex="^[+-]?(((\\d+\\.\\d*)|(\\.\\d+))|(\\d+))([eE][+-]?\\d+)?$";
+        return Pattern.matches(regex,s);
     }
 }
